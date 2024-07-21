55727dd15c8a56138cc963b98dfdf92ef24755ee
Rahelaymer <rahelaymere6@gmal.com>
2024-07-22 00:00:20 +0300

    login

---
+++ app/login/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Login from '../componenets/Login'
+import { useRouter } from 'next/router';
+
+const Loginpage = () => {
+  
+  return (
+    <div><Login/></div>
+  )
+}
+
+export default Loginpage
